a7fbc50384aa6c134ba17962e7a4cc9b392997a2
Siddarth Vijay <vsiddarthvijay@gmail.com>
2020-05-17 01:01:30 +0530

    added shortened hash of curator

--- x/kiraHub/transactions/createOrderBook/keeper.go
+++ x/kiraHub/transactions/createOrderBook/keeper.go
@@ -2,13 +2,12 @@ package createOrderBook
 
 import (
 	"encoding/hex"
-	"strconv"
 
-	sdk "github.com/KiraCore/cosmos-sdk/types"
 	"github.com/KiraCore/cosmos-sdk/codec"
+	sdk "github.com/KiraCore/cosmos-sdk/types"
 	"github.com/KiraCore/sekai/types"
-	"golang.org/x/crypto/blake2b"
 
+	"golang.org/x/crypto/blake2b"
 )
 
 type Keeper struct {
@@ -24,7 +23,6 @@ func NewKeeper(cdc *codec.Codec, storeKey sdk.StoreKey) Keeper {
 }
 
 func (k Keeper) CreateOrderBook(ctx sdk.Context, quote sdk.Coins, base sdk.Coins, curator sdk.AccAddress, mnemonic string) {
-
 	var orderbook = types.OrderBook{}
 
 	orderbook.Quote = quote
@@ -32,11 +30,14 @@ func (k Keeper) CreateOrderBook(ctx sdk.Context, quote sdk.Coins, base sdk.Coins
 	orderbook.Curator = curator
 	orderbook.Mnemonic = mnemonic
 
+	// This is the definitions of the lens of the shortened hashes
+	numberOfBytes := 4
+	numberOfCharacters := 2*numberOfBytes
 
-	var curatorHash = blake2b.Sum256(curator)
-
-	var id = hex.EncodeToString(curatorHash[len(curatorHash) - 4:])
-
+	// Creating the hashes of the parts of the ID
+	hashOfCurator := blake2b.Sum256([]byte(curator))
+	hashInStringOfCurator := hex.EncodeToString(hashOfCurator[:])
+	idHashInStringOfCurator := hashInStringOfCurator[len(hashInStringOfCurator) - numberOfCharacters:]
 
 	store := ctx.KVStore(k.storeKey)
 
