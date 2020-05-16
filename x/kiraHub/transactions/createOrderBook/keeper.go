2ae980baab1e33023926126482d5b1fe88c920cd
Siddarth Vijay <vsiddarthvijay@gmail.com>
2020-05-17 02:16:20 +0530

    finished making the id with the shortened hashes still need to implement index though

--- x/kiraHub/transactions/createOrderBook/keeper.go
+++ x/kiraHub/transactions/createOrderBook/keeper.go
@@ -2,6 +2,7 @@ package createOrderBook
 
 import (
 	"encoding/hex"
+	"strings"
 
 	"github.com/KiraCore/cosmos-sdk/codec"
 	sdk "github.com/KiraCore/cosmos-sdk/types"
@@ -30,14 +31,34 @@ func (k Keeper) CreateOrderBook(ctx sdk.Context, quote sdk.Coins, base sdk.Coins
 	orderbook.Curator = curator
 	orderbook.Mnemonic = mnemonic
 
+	// ARJUN CHANGE THIS TO THE DYNAMIC INDEX PULLED FROM THE KVSTORE
+	// var last_order_book_index = 1
+
 	// This is the definitions of the lens of the shortened hashes
-	numberOfBytes := 4
-	numberOfCharacters := 2*numberOfBytes
+	var numberOfBytes = 4
+	var numberOfCharacters = 2 * numberOfBytes
 
 	// Creating the hashes of the parts of the ID
 	hashOfCurator := blake2b.Sum256([]byte(curator))
 	hashInStringOfCurator := hex.EncodeToString(hashOfCurator[:])
 	idHashInStringOfCurator := hashInStringOfCurator[len(hashInStringOfCurator) - numberOfCharacters:]
+
+	hashOfBase := blake2b.Sum256([]byte(base))
+	hashInStringOfBase := hex.EncodeToString(hashOfBase[:])
+	idHashInStringOfBase := hashInStringOfBase[len(hashInStringOfBase) - numberOfCharacters:]
+
+	hashOfQuote := blake2b.Sum256([]byte(quote))
+	hashInStringOfQuote := hex.EncodeToString(hashOfQuote[:])
+	idHashInStringOfQuote := hashInStringOfQuote[len(hashInStringOfQuote) - numberOfCharacters:]
+
+	var ID strings.Builder
+
+	ID.WriteString(idHashInStringOfCurator)
+	ID.WriteString(idHashInStringOfBase)
+	ID.WriteString(idHashInStringOfQuote)
+	// Still need to add the functionalities of last_order_book_index
+
+	id := ID.String()
 
 	store := ctx.KVStore(k.storeKey)
 
