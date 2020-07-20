4cfa13062d6e362a3c948df443ea0170f7adfe64
Jonathan Gimeno <jgimeno@gmail.com>
2020-07-20 20:32:54 +0200

    overwrite staking module to prepare consensus scenario

---
+++ x/staking/module.go
@@ -0,0 +1,29 @@
+package staking
+
+import (
+	"github.com/KiraCore/cosmos-sdk/codec"
+	"github.com/KiraCore/cosmos-sdk/types/module"
+	"github.com/KiraCore/cosmos-sdk/x/auth"
+	"github.com/KiraCore/cosmos-sdk/x/staking"
+	"github.com/KiraCore/cosmos-sdk/x/staking/types"
+)
+
+var (
+	_ module.AppModule = AppModule{}
+)
+
+// AppModule extends the cosmos SDK staking.
+type AppModule struct {
+	staking.AppModule
+}
+
+func NewAppModule(
+	cdc codec.Marshaler,
+	keeper staking.Keeper,
+	ak auth.AccountKeeper,
+	bk types.BankKeeper,
+) AppModule {
+	return AppModule{
+		AppModule: staking.NewAppModule(cdc, keeper, ak, bk),
+	}
+}
