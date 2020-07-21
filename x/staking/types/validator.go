2e9355c49678ea28a6493745e1cb4454907e555f
Kira Dev <codingkira@gmail.com>
2020-07-21 23:40:19 +0200

    add validator

--- x/staking/types/validator.go
+++ x/staking/types/validator.go
@@ -1,4 +1,15 @@
 package types
 
+import sdk "github.com/KiraCore/cosmos-sdk/types"
+
 type Validator struct {
+	Moniker  string
+	Website  string
+	Social   string
+	Identity string
+
+	Comission sdk.Dec
+
+	ValKey sdk.ValAddress
+	PubKey sdk.AccAddress
 }
