cf243da3ffd8d42973974b80193373fe14e27e0f
Kira Dev <codingkira@gmail.com>
2020-08-02 12:31:33 +0200

    create the application client test

---
+++ x/staking/client/cli/cli_test.go
@@ -0,0 +1,32 @@
+package cli
+
+import (
+	"github.com/stretchr/testify/suite"
+
+	"github.com/KiraCore/cosmos-sdk/testutil/network"
+)
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	cfg     network.Config
+	network *network.Network
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+
+	cfg := network.DefaultConfig()
+	cfg.NumValidators = 1
+
+	s.cfg = cfg
+	s.network = network.New(s.T(), cfg)
+
+	_, err := s.network.WaitForHeight(1)
+	s.Require().NoError(err)
+}
+
+func (s *IntegrationTestSuite) TearDownSuite() {
+	s.T().Log("tearing down integration test suite")
+	s.network.Cleanup()
+}
