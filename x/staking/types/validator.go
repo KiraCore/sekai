fdd2c9aeb1b0277abea0cb78cf2a0e1117b4dab0
Kira Dev <codingkira@gmail.com>
2020-08-07 20:04:02 +0200

    create validator

--- x/staking/types/validator.go
+++ x/staking/types/validator.go
@@ -4,28 +4,16 @@ import (
 	sdk "github.com/KiraCore/cosmos-sdk/types"
 )
 
-type Validator struct {
-	Moniker  string
-	Website  string
-	Social   string
-	Identity string
-
-	Comission sdk.Dec
-
-	ValKey sdk.ValAddress
-	PubKey sdk.AccAddress
-}
-
 // NewValidator generates new Validator.
 func NewValidator(moniker string, website string, social string, identity string, comission sdk.Dec, valKey sdk.ValAddress, pubKey sdk.AccAddress) (Validator, error) {
 	v := Validator{
-		Moniker:   moniker,
-		Website:   website,
-		Social:    social,
-		Identity:  identity,
-		Comission: comission,
-		ValKey:    valKey,
-		PubKey:    pubKey,
+		Moniker:    moniker,
+		Website:    website,
+		Social:     social,
+		Identity:   identity,
+		Commission: comission,
+		ValKey:     valKey,
+		PubKey:     pubKey,
 	}
 
 	err := v.Validate()
