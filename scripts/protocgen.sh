db1c7d59b13ff42fda9c45c336c8102fdc5fd901
Kira Dev <codingkira@gmail.com>
2020-08-21 20:58:50 +0200

    add generated gov types

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -12,5 +12,5 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
 done
 
 # move proto files to the right places
-cp -r github.com/cosmos/cosmos-sdk/* ./
+cp -r github.com/KiraCore/sekai/* ./
 rm -rf github.com
