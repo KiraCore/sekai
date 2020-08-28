b66db7da08e4fde64a64946943b1bfff2cf72259
junkai121 <junkai121@outlook.com>
2020-08-29 00:49:19 +1000

    create orderbook basic path msg validation test

---
+++ x/ixp/types/msg_test.go
@@ -0,0 +1,46 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/KiraCore/sekai/app"
+	ixptypes "github.com/KiraCore/sekai/x/ixp/types"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgCreateOrderbook_ValidateBasic(t *testing.T) {
+	app.SetConfig()
+
+	kiraAddr1, err := types.AccAddressFromBech32("kira1da22wd7slpxpptasczs679mr5c8xtucqdzxc3n")
+	require.NoError(t, err)
+
+	tests := []struct {
+		name          string
+		constructor   func() (*ixptypes.MsgCreateOrderBook, error)
+		validationErr string
+	}{
+		{
+			name: "basic path test",
+			constructor: func() (*ixptypes.MsgCreateOrderBook, error) {
+				return ixptypes.NewMsgCreateOrderBook("base", "quote", "mnemonic", kiraAddr1)
+			},
+			validationErr: "",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			msg, err := tt.constructor()
+			require.Equal(t, err, nil)
+
+			err = msg.ValidateBasic()
+			if tt.validationErr != "" {
+				require.NotEqual(t, err, nil)
+				require.Contains(t, err.Error(), tt.validationErr)
+			} else {
+				require.Equal(t, err, nil)
+			}
+		})
+	}
+}
