e1d8c11fbd8da179cb776c703fd68ded05a64116
junkai121 <junkai121@outlook.com>
2020-09-08 22:11:25 +1000

    Write handler test for SetNetworkProperties handler

--- x/gov/handler_test.go
+++ x/gov/handler_test.go
@@ -251,6 +251,75 @@ func TestNewHandler_SetPermissions_ProposerHasRoleSudo(t *testing.T) {
 	}
 }
 
+func TestNewHandler_SetNetworkProperties(t *testing.T) {
+	changeFeeAddr, err := sdk.AccAddressFromBech32("kira15ky9du8a2wlstz6fpx3p4mqpjyrm5cgqzp4f3d")
+	require.NoError(t, err)
+
+	sudoAddr, err := sdk.AccAddressFromBech32("kira1alzyfq40zjsveat87jlg8jxetwqmr0a29sgd0f")
+	require.NoError(t, err)
+
+	tests := []struct {
+		name       string
+		msg        sdk.Msg
+		desiredErr string
+	}{
+		{
+			name: "Success run with ChangeTxFee permission",
+			msg: &types.MsgSetNetworkProperties{
+				NetworkProperties: &types.NetworkProperties{
+					MinTxFee: 100,
+					MaxTxFee: 1000,
+				},
+				Proposer: changeFeeAddr,
+			},
+			desiredErr: "",
+		},
+		{
+			name: "Failure run without ChangeTxFee permission",
+			msg: &types.MsgSetNetworkProperties{
+				NetworkProperties: &types.NetworkProperties{
+					MinTxFee: 100,
+					MaxTxFee: 1000,
+				},
+				Proposer: sudoAddr,
+			},
+			desiredErr: "not enough permissions",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			app := simapp.Setup(false)
+			ctx := app.NewContext(false, tmproto.Header{})
+
+			// First we set Role Sudo to proposer Actor
+			proposerActor := types.NewDefaultActor(sudoAddr)
+			proposerActor.SetRole(types.RoleSudo)
+			require.NoError(t, err)
+			app.CustomGovKeeper.SaveNetworkActor(ctx, proposerActor)
+
+			handler := gov.NewHandler(app.CustomGovKeeper)
+
+			// set change fee permission to addr
+			_, err = handler(ctx, &types.MsgWhitelistPermissions{
+				Proposer:   sudoAddr,
+				Address:    changeFeeAddr,
+				Permission: uint32(types.PermChangeTxFee),
+			})
+			require.NoError(t, err)
+
+			_, err = handler(ctx, tt.msg)
+			if tt.desiredErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.desiredErr)
+			}
+		})
+	}
+}
+
 func setSetPermissionsToAddr(t *testing.T, app *simapp.SimApp, ctx sdk.Context, addr sdk.AccAddress) error {
 	proposerActor := types.NewDefaultActor(addr)
 	err := proposerActor.Permissions.AddToWhitelist(types.PermSetPermissions)
