cd829fffcd413c3b2c895b3ffd21668520609356
Kira Dev <codingkira@gmail.com>
2020-09-10 02:00:12 +0200

    Add councilor by moniker logic.

--- x/gov/keeper/councilor.go
+++ x/gov/keeper/councilor.go
@@ -17,11 +17,19 @@ func GetCouncilorKey(address sdk.AccAddress) []byte {
 	return append(CouncilorsKey, address.Bytes()...)
 }
 
+func GetCouncilorByMonikerKey(moniker string) []byte {
+	return append(CouncilorsByMonikerKey, []byte(moniker)...)
+}
+
 func (k Keeper) SaveCouncilor(ctx sdk.Context, councilor types.Councilor) {
 	prefixStore := prefix.NewStore(ctx.KVStore(k.storeKey), KeyPrefixCouncilorIdentityRegistry)
 
 	bz := k.cdc.MustMarshalBinaryBare(&councilor)
-	prefixStore.Set(GetCouncilorKey(councilor.Address), bz)
+
+	councilorKey := GetCouncilorKey(councilor.Address)
+
+	prefixStore.Set(councilorKey, bz)
+	prefixStore.Set(GetCouncilorByMonikerKey(councilor.Moniker), councilorKey)
 }
 
 func (k Keeper) GetCouncilor(ctx sdk.Context, address sdk.AccAddress) (types.Councilor, error) {
@@ -36,4 +44,17 @@ func (k Keeper) GetCouncilor(ctx sdk.Context, address sdk.AccAddress) (types.Cou
 	k.cdc.MustUnmarshalBinaryBare(bz, &co)
 
 	return co, nil
+}
+
+func (k Keeper) GetCouncilorByMoniker(ctx sdk.Context, moniker string) (types.Councilor, error) {
+	prefixStore := prefix.NewStore(ctx.KVStore(k.storeKey), KeyPrefixCouncilorIdentityRegistry)
+
+	councilorKey := prefixStore.Get(GetCouncilorByMonikerKey(moniker))
+
+	bz := prefixStore.Get(councilorKey)
+	var councilor types.Councilor
+
+	k.cdc.MustUnmarshalBinaryBare(bz, &councilor)
+
+	return councilor, nil
 }
