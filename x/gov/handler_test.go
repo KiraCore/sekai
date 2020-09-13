4498df3c27037b7e509a3b9f1071e9a22dc218d0
Kira Dev <codingkira@gmail.com>
2020-09-13 12:53:58 +0200

    Add handler test.

--- x/gov/handler_test.go
+++ x/gov/handler_test.go
@@ -509,6 +509,35 @@ func TestHandler_BlacklistRolePermissions_Errors(t *testing.T) {
 	}
 }
 
+func TestHandler_BlacklistRolePermissions(t *testing.T) {
+	addr, err := sdk.AccAddressFromBech32("kira15ky9du8a2wlstz6fpx3p4mqpjyrm5cgqzp4f3d")
+	require.NoError(t, err)
+
+	app := simapp.Setup(false)
+	ctx := app.NewContext(false, tmproto.Header{})
+
+	err = setPermissionToAddr(t, app, ctx, addr, types.PermSetPermissions)
+	require.NoError(t, err)
+
+	perms, err := app.CustomGovKeeper.GetPermissionsForRole(ctx, types.RoleValidator)
+	require.NoError(t, err)
+	require.False(t, perms.IsBlacklisted(types.PermSetPermissions))
+
+	msg := types.NewMsgBlacklistRolePermission(
+		addr,
+		uint32(types.RoleValidator),
+		uint32(types.PermSetPermissions),
+	)
+
+	handler := gov.NewHandler(app.CustomGovKeeper)
+	_, err = handler(ctx, msg)
+	require.NoError(t, err)
+
+	perms, err = app.CustomGovKeeper.GetPermissionsForRole(ctx, types.RoleValidator)
+	require.NoError(t, err)
+	require.True(t, perms.IsBlacklisted(types.PermSetPermissions))
+}
+
 func setPermissionToAddr(t *testing.T, app *simapp.SimApp, ctx sdk.Context, addr sdk.AccAddress, perm types.PermValue) error {
 	proposerActor := types.NewDefaultActor(addr)
 	err := proposerActor.Permissions.AddToWhitelist(perm)
