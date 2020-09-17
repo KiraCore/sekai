4f57c9690e78d956f469e6342a9fc489c4bd7b7d
Kira Dev <codingkira@gmail.com>
2020-09-17 22:50:39 +0200

    update sekaidtextsetup

--- sekaidtestsetup.sh
+++ sekaidtestsetup.sh
@@ -4,8 +4,8 @@ rm -rf $HOME/.sekaid/
 
 cd $HOME
 
-sekaid init --chain-id=testing testing
-sekaid keys add validator --keyring-backend=test
-sekaid add-genesis-account $(sekaid keys show validator -a --keyring-backend=test) 1000000000stake,1000000000validatortoken --keyring-backend=test
-sekaid gentx-claim validator --keyring-backend=test --moniker="hello"
-sekaid start
+sekaid init --chain-id=testing testing --home=/Users/jgimeno/.sekaid
+sekaid keys add validator --keyring-backend=test --home=/Users/jgimeno/.sekaid
+sekaid add-genesis-account $(sekaid keys show validator -a --home=/Users/jgimeno/.sekaid --keyring-backend=test) 1000000000stake,1000000000validatortoken  --home=/Users/jgimeno/.sekaid
+sekaid gentx-claim validator --keyring-backend=test --moniker="hello" --home=/Users/jgimeno/.sekaid
+sekaid start --home=/Users/jgimeno/.sekaid
