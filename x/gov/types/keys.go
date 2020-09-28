dfb3e9f1de3e4198d9f71f961d6adfaa94fceeb7
Kira Dev <codingkira@gmail.com>
2020-09-28 23:47:23 +0200

    Move Permission Types to Enum.

--- x/gov/types/keys.go
+++ x/gov/types/keys.go
@@ -10,18 +10,11 @@ var (
 	RoleUndefined Role = 0x0
 	RoleSudo      Role = 0x1
 	RoleValidator Role = 0x2
-
-	PermClaimValidator PermValue = 1
-	PermSetPermissions PermValue = 2
-	PermClaimCouncilor PermValue = 3
 )
 
 // Role represents a Role in the registry.
 type Role uint64
 type Roles []uint64
-
-// PermValue represents a single permission value, like claim-role-validator.
-type PermValue uint32
 
 // RoleToKey returns bytes to be used as a key for a given capability index.
 func RoleToKey(index Role) []byte {
