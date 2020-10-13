2315dc02a8226f2c597696c5b41087acbbe82e69
fantasyup <fantasy99483@gmail.com>
2020-10-13 21:48:17 +1000

    add tx encode API

--- INTERX/gateway/tx.go
+++ INTERX/gateway/tx.go
@@ -1,11 +1,14 @@
 package gateway
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
 	interx "github.com/KiraCore/sekai/INTERX/config"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	auth "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 )
@@ -13,14 +16,17 @@ import (
 const (
 	postTransaction      = "/api/cosmos/txs"
 	queryTransactionHash = "/api/cosmos/txs"
+	encodeTransaction    = "/api/cosmos/txs/encode"
 )
 
 // RegisterTxRoutes registers query routers.
 func RegisterTxRoutes(r *mux.Router, gwCosmosmux *runtime.ServeMux, rpcAddr string) {
 	r.HandleFunc(postTransaction, PostTxRequest(rpcAddr)).Methods(POST)
+	r.HandleFunc(encodeTransaction, EncodeTransaction(rpcAddr)).Methods(POST)
 	r.HandleFunc("/api/cosmos/txs/{hash}", QueryTxHashRequest(rpcAddr)).Methods(GET)
 
 	AddRPCMethod(POST, postTransaction, "This is an API to post transaction.")
+	AddRPCMethod(POST, encodeTransaction, "This is an API to encode transaction.")
 	AddRPCMethod(GET, queryTransactionHash, "This is an API to query transaction from transaction hash.")
 }
 
@@ -105,5 +111,46 @@ func QueryTxHashRequest(rpcAddr string) http.HandlerFunc {
 		} else {
 			ServeRPC(w, request, response, rpcAddr, statusCode)
 		}
+	}
+}
+
+// EncodeTransaction is a function to encode unsigned transaction.
+func EncodeTransaction(rpcAddr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		request := GetInterxRequest(r)
+
+		if !rpcMethods[POST][encodeTransaction].Enabled {
+			ServeError(w, request, rpcAddr, 0, "", "", http.StatusForbidden)
+			return
+		}
+
+		// TxEncodeReq defines a tx to be encoded.
+		type TxEncodeReq struct {
+			ChainID       string      `json:"chain_id" yaml:"chain_id"`
+			AccountNumber uint64      `json:"account_number" yaml:"account_number"`
+			Sequence      uint64      `json:"sequence" yaml:"sequence"`
+			Tx            types.StdTx `json:"tx" yaml:"tx"`
+		}
+		var req TxEncodeReq
+
+		err := interx.EncodingCg.Amino.UnmarshalJSON(request.Params, &req)
+		if err != nil {
+			ServeError(w, request, rpcAddr, 0, "failed to unmarshal", err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		signBytes := auth.StdSignBytes(req.ChainID, req.AccountNumber, req.Sequence, 0, req.Tx.Fee, req.Tx.Msgs, req.Tx.Memo)
+
+		// TxEncodeResponse defines base64 encoded transaction.
+		type TxEncodeResponse struct {
+			Tx string `json:"tx" yaml:"tx"`
+		}
+
+		response := GetResponseFormat(request, rpcAddr)
+		response.Response = TxEncodeResponse{
+			Tx: base64.StdEncoding.EncodeToString(signBytes),
+		}
+
+		WrapResponse(w, *response, 200)
 	}
 }
