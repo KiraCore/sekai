f90951371f6dfa1be9c08abcff36d7fef5a5bf7e
Kira Dev <codingkira@gmail.com>
2020-10-25 20:22:08 +0100

    Add error checks.

--- x/gov/types/quorum_test.go
+++ x/gov/types/quorum_test.go
@@ -1,10 +1,42 @@
 package types
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestIsQuorum_Errors(t *testing.T) {
+	tests := []struct {
+		name                      string
+		percentage, votes, voters uint64
+		expectedErr               error
+	}{
+		{
+			name:        "more votes than total voters",
+			percentage:  66,
+			votes:       11,
+			voters:      10,
+			expectedErr: fmt.Errorf("there is more votes than voters"),
+		},
+		{
+			name:        "invalid quorum",
+			percentage:  101,
+			votes:       7,
+			voters:      10,
+			expectedErr: fmt.Errorf("quorum cannot be bigger than 100"),
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := IsQuorum(tt.percentage, tt.votes, tt.voters)
+			require.EqualError(t, err, tt.expectedErr.Error())
+		})
+	}
+}
 
 func TestIsQuorum(t *testing.T) {
 	tests := []struct {
@@ -31,7 +63,9 @@ func TestIsQuorum(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			require.Equal(t, tt.reached, IsQuorum(tt.percentage, tt.votes, tt.voters))
+			quorum, err := IsQuorum(tt.percentage, tt.votes, tt.voters)
+			require.NoError(t, err)
+			require.Equal(t, tt.reached, quorum)
 		})
 	}
 }
