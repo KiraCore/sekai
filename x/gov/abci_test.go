80066ddc5b2719fb9725919941c618b21096dd7a
Kira Dev <codingkira@gmail.com>
2020-10-25 20:42:43 +0100

    add vote quorum into network properties

--- x/gov/abci_test.go
+++ x/gov/abci_test.go
@@ -2,5 +2,12 @@ package gov
 
 import "testing"
 
-func TestEndBlocker_ItIsQuorum(t *testing.T) {
+func TestEndBlocker(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "",
+		},
+	}
 }
