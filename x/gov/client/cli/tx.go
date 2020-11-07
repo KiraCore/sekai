72d1e4ac818451a86c542f8212517dab1c531712
Kira Dev <codingkira@gmail.com>
2020-11-07 10:28:04 +0100

    Add vote option CLI.

--- x/gov/client/cli/tx.go
+++ x/gov/client/cli/tx.go
@@ -46,7 +46,7 @@ func NewTxCmd() *cobra.Command {
 	txCmd.AddCommand(NewTxProposalCmds())
 	txCmd.AddCommand(NewTxRoleCmds())
 	txCmd.AddCommand(NewTxPermissionCmds())
-	txCmd.AddCommand(		GetTxSetNetworkProperties(),
+	txCmd.AddCommand(GetTxSetNetworkProperties(),
 		GetTxSetExecutionFee(),
 	)
 
@@ -590,6 +590,47 @@ func GetTxProposalAssignPermission() *cobra.Command {
 				clientCtx.FromAddress,
 				addr,
 				types.PermValue(perm),
+			)
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	cmd.Flags().String(cli.FlagAddr, "", "the address to set permissions")
+
+	_ = cmd.MarkFlagRequired(flags.FlagFrom)
+	_ = cmd.MarkFlagRequired(cli.FlagAddr)
+
+	return cmd
+}
+
+func GetTxVoteProposal() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "vote proposal-id vote-option",
+		Short: "Create a proposal to assign a permission to an address.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			clientCtx, err := client.ReadTxCommandFlags(clientCtx, cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			proposalID, err := strconv.Atoi(args[0])
+			if err != nil {
+				return fmt.Errorf("invalid proposal ID: %w", err)
+			}
+
+			voteOption, err := strconv.Atoi(args[1])
+			if err != nil {
+				return fmt.Errorf("invalid vote option: %w", err)
+			}
+
+			msg := types.NewMsgVoteProposal(
+				uint64(proposalID),
+				clientCtx.FromAddress,
+				types.VoteOption(voteOption),
 			)
 
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
