5db2e1de22085550ce2e1e961c4c3c6c32a19f74
Kira Dev <codingkira@gmail.com>
2020-11-08 13:48:25 +0100

    Add proposal generic.

---
+++ x/gov/types/content.go
@@ -0,0 +1,5 @@
+package types
+
+type Content interface {
+	ProposalType() string
+}
--- x/gov/types/proposal.go
+++ x/gov/types/proposal.go
@@ -6,6 +6,14 @@ import (
 	"github.com/cosmos/cosmos-sdk/types"
 )
 
+const AssignPermissionProposalType = "AssignPermission"
+
+var _ Content = &AssignPermissionProposal{}
+
+func (m *AssignPermissionProposal) ProposalType() string {
+	return AssignPermissionProposalType
+}
+
 func NewProposalAssignPermission(
 	proposalID uint64,
 	address types.AccAddress,
@@ -15,12 +23,12 @@ func NewProposalAssignPermission(
 	enactmentEndTime time.Time,
 ) ProposalAssignPermission {
 	return ProposalAssignPermission{
-		ProposalId:      proposalID,
-		Address:         address,
-		Permission:      uint32(permission),
-		VotingStartTime: votingStartTime,
-		VotingEndTime:   votingEndTime,
+		ProposalId:       proposalID,
+		Address:          address,
+		Permission:       uint32(permission),
+		VotingStartTime:  votingStartTime,
+		VotingEndTime:    votingEndTime,
 		EnactmentEndTime: enactmentEndTime,
-		Result:          Pending,
+		Result:           Pending,
 	}
 }
