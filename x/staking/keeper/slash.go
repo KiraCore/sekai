27a111d27aa70154bff2f4fd61ab8e7e12a45324
junkai121 <junkai121@outlook.com>
2020-12-17 13:33:20 +1000

    staking module: add required functions as declaration with TODO

--- x/staking/keeper/slash.go
+++ x/staking/keeper/slash.go
@@ -15,3 +15,8 @@ func (k Keeper) Activate(ctx sdk.Context, consAddr sdk.ConsAddress) {
 	// validator.Inactivated = false
 	// k.SetValidator(ctx, validator)
 }
+
+// slash the validator and delegators of the validator, specifying offence height, offence power, and slash fraction
+func (k Keeper) Inactivate(sdk.Context, sdk.ConsAddress) { // inactivate a validator
+	// TODO: don't do anything for now, implement this
+}
