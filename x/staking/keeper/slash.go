991e56e6ed1dec029bee9059a6ba756e9b416579
Kira Dev <codingkira@gmail.com>
2020-12-24 20:16:49 +0100

    refactor tests

--- x/staking/keeper/slash.go
+++ x/staking/keeper/slash.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	customstakingtypes "github.com/KiraCore/sekai/x/staking/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -22,20 +23,21 @@ func (k Keeper) Inactivate(sdk.Context, sdk.ConsAddress) { // inactivate a valid
 }
 
 // Pause a validator
-func (k Keeper) Pause(ctx sdk.Context, consAddr sdk.ValAddress) {
-	// TODO: don't do anything for now, implement this
-	// validator := k.GetValidatorByConsAddr(ctx, consAddr)
-	// if !validator.Inactivated {
-	// 	panic(fmt.Sprintf("cannot unjail already inactivated validator, validator: %v\n", validator))
-	// }
+func (k Keeper) Pause(ctx sdk.Context, valAddress sdk.ValAddress) error {
+	validator, err := k.GetValidator(ctx, valAddress)
+	if err != nil {
+		return err
+	}
 
-	// validator.Inactivated = false
-	// k.SetValidator(ctx, validator)
+	validator.Status = customstakingtypes.Paused
+	k.AddValidator(ctx, validator)
+
+	return nil
 }
 
 // Unpause unpause the validator
-func (k Keeper) Unpause(sdk.Context, sdk.ValAddress) { // inactivate a validator
-	// TODO: don't do anything for now, implement this
+func (k Keeper) Unpause(sdk.Context, sdk.ValAddress) error { // inactivate a validator
+	return nil
 }
 
 // TODO: should take care of relation between Activate / Pause
