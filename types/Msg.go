b9e167e25e90b699df159950351e94751c448901
junkai121 <junkai121@outlook.com>
2021-01-12 13:22:30 +1000

    remove old module messages registration

--- types/Msg.go
+++ types/Msg.go
@@ -2,13 +2,6 @@ package types
 
 import (
 	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
-	crisis "github.com/cosmos/cosmos-sdk/x/crisis/types"
-	distribution "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	evidence "github.com/cosmos/cosmos-sdk/x/evidence/types"
-	gov "github.com/cosmos/cosmos-sdk/x/gov/types"
-	"github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
-	slashing "github.com/cosmos/cosmos-sdk/x/slashing/types"
-	staking "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // custom msg types
@@ -53,47 +46,31 @@ const (
 
 // MsgFuncIDMapping defines function_id mapping
 var MsgFuncIDMapping = map[string]int64{
-	bank.TypeMsgSend:                                1,
-	bank.TypeMsgMultiSend:                           2,
-	(crisis.MsgVerifyInvariant{}).Type():            3,
-	distribution.TypeMsgSetWithdrawAddress:          4,
-	distribution.TypeMsgWithdrawDelegatorReward:     5,
-	distribution.TypeMsgWithdrawValidatorCommission: 6,
-	distribution.TypeMsgFundCommunityPool:           7,
-	evidence.TypeMsgSubmitEvidence:                  8,
-	gov.TypeMsgSubmitProposal:                       9,
-	gov.TypeMsgDeposit:                              10,
-	gov.TypeMsgVote:                                 11,
-	types.TypeMsgTransfer:                           12,
-	slashing.TypeMsgUnjail:                          13,
-	staking.TypeMsgCreateValidator:                  14,
-	staking.TypeMsgEditValidator:                    15,
-	staking.TypeMsgDelegate:                         16,
-	staking.TypeMsgBeginRedelegate:                  17,
-	staking.TypeMsgUndelegate:                       18,
-	MsgTypeSetNetworkProperties:                     19,
-	MsgTypeSetExecutionFee:                          20,
-	MsgTypeProposalAssignPermission:                 21,
-	MsgTypeProposalSetNetworkProperty:               22,
-	MsgTypeProposalUpsertDataRegistry:               23,
-	MsgTypeVoteProposal:                             24,
-	MsgTypeClaimCouncilor:                           25,
-	MsgTypeWhitelistPermissions:                     26,
-	MsgTypeBlacklistPermissions:                     27,
-	MsgTypeCreateRole:                               28,
-	MsgTypeAssignRole:                               29,
-	MsgTypeRemoveRole:                               30,
-	MsgTypeWhitelistRolePermission:                  31,
-	MsgTypeBlacklistRolePermission:                  32,
-	MsgTypeRemoveWhitelistRolePermission:            33,
-	MsgTypeRemoveBlacklistRolePermission:            34,
-	MsgTypeClaimValidator:                           35,
-	MsgTypeUpsertTokenAlias:                         36,
-	MsgTypeUpsertTokenRate:                          37,
-	MsgTypeProposalUpsertTokenAlias:                 38,
-	MsgProposalUpsertTokenAliasType:                 39,
-	MsgProposalUpsertTokenRatesType:                 40,
-	MsgTypeActivate:                                 41,
-	MsgTypePause:                                    42,
-	MsgTypeUnpause:                                  43,
+	bank.TypeMsgSend:                     1,
+	bank.TypeMsgMultiSend:                2,
+	MsgTypeSetNetworkProperties:          19,
+	MsgTypeSetExecutionFee:               20,
+	MsgTypeProposalAssignPermission:      21,
+	MsgTypeProposalSetNetworkProperty:    22,
+	MsgTypeProposalUpsertDataRegistry:    23,
+	MsgTypeVoteProposal:                  24,
+	MsgTypeClaimCouncilor:                25,
+	MsgTypeWhitelistPermissions:          26,
+	MsgTypeBlacklistPermissions:          27,
+	MsgTypeCreateRole:                    28,
+	MsgTypeAssignRole:                    29,
+	MsgTypeRemoveRole:                    30,
+	MsgTypeWhitelistRolePermission:       31,
+	MsgTypeBlacklistRolePermission:       32,
+	MsgTypeRemoveWhitelistRolePermission: 33,
+	MsgTypeRemoveBlacklistRolePermission: 34,
+	MsgTypeClaimValidator:                35,
+	MsgTypeUpsertTokenAlias:              36,
+	MsgTypeUpsertTokenRate:               37,
+	MsgTypeProposalUpsertTokenAlias:      38,
+	MsgProposalUpsertTokenAliasType:      39,
+	MsgProposalUpsertTokenRatesType:      40,
+	MsgTypeActivate:                      41,
+	MsgTypePause:                         42,
+	MsgTypeUnpause:                       43,
 }
