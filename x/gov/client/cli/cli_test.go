dec35e088fdcde0a7d174056dd867dbe2c07edd3
junkai121 <junkai121@outlook.com>
2021-02-02 21:57:49 +1000

    Write happy path test prototype for proposal and vote SetPoorNetworkMessages and query

--- x/gov/client/cli/cli_test.go
+++ x/gov/client/cli/cli_test.go
@@ -8,15 +8,14 @@ import (
 	"github.com/KiraCore/sekai/x/gov/client/cli"
 	customgovtypes "github.com/KiraCore/sekai/x/gov/types"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/testutil"
-
 	"github.com/KiraCore/sekai/app"
 	"github.com/KiraCore/sekai/simapp"
 	"github.com/KiraCore/sekai/testutil/network"
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/client"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	"github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/testutil"
 	"github.com/stretchr/testify/suite"
 	dbm "github.com/tendermint/tm-db"
 )
@@ -137,6 +136,54 @@ func (s IntegrationTestSuite) TestClaimCouncilor_HappyPath() {
 	s.Require().NoError(err)
 	s.Require().Equal(val.Moniker, councilorByMoniker.Moniker)
 	s.Require().Equal(val.Address, councilorByMoniker.Address)
+}
+
+func (s IntegrationTestSuite) TestProposalAndVoteSetPoorNetworkMessages_HappyPath() {
+	val := s.network.Validators[0]
+	// # create proposal for setting poor network msgs
+	// sekaid tx customgov proposal set-poor-network-msgs AAA,BBB --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=1000ukex --yes
+	s.SetPoorNetworkMessages("AAA,BBB")
+	// # query for proposals
+	// sekaid query customgov proposals
+	s.QueryProposals()
+	// # set permission to vote on proposal
+	// sekaid tx customgov permission whitelist-permission --permission=19 --addr=$(sekaid keys show -a validator --keyring-backend=test --home=$HOME/.sekaid) --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes
+	s.WhitelistPermission(val.Address, "19") // 19 is permission for vote on poor network message set proposal
+	// # vote on the proposal
+	// sekaid tx customgov proposal vote 1 1 --from validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes
+	s.VoteWithValidator0(1, customgovtypes.OptionYes)
+	// # check votes
+	// sekaid query customgov votes 1
+	s.QueryProposalVotes(1)
+	// # wait until vote end time finish
+	// sekaid query customgov proposals
+	// TODO: this takes long time and for now skip waiting
+	// # query poor network messages
+	// sekaid query customgov poor-network-messages
+	s.QueryPoorNetworkMessages()
+}
+
+func (s IntegrationTestSuite) TestProposalAndVotePoorNetworkMaxBankSend_HappyPath() {
+	// TODO: complete scenarios
+	// # try setting network property by governance to allow more amount sending
+	// sekaid tx customgov proposal set-network-property POOR_NETWORK_MAX_BANK_SEND 100000000 --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes
+	// sekaid tx customgov proposal vote 1 1 --from validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes
+	// # try sending after modification of poor network bank send param
+	// sekaid tx bank send validator $(sekaid keys show -a validator --keyring-backend=test --home=$HOME/.sekaid) 100000000ukex --keyring-backend=test --chain-id=testing --fees=100ukex --home=$HOME/.sekaid --yes
+}
+
+func (s IntegrationTestSuite) TestPoorNetworkRestrictions_HappyPath() {
+	// TODO: complete scenarios
+	// # whitelist permission for modifying network properties
+	// sekaid tx customgov permission whitelist-permission --from validator --keyring-backend=test --permission=7 --addr=$(sekaid keys show -a validator --keyring-backend=test --home=$HOME/.sekaid) --chain-id=testing --fees=100ukex --home=$HOME/.sekaid --yes
+	// # test poor network messages after modifying min_validators section
+	// sekaid tx customgov set-network-properties --from validator --min_validators="2" --keyring-backend=test --chain-id=testing --fees=100ukex --home=$HOME/.sekaid --yes
+	// # set permission for upsert token rate
+	// sekaid tx customgov permission whitelist-permission --from validator --keyring-backend=test --permission=$PermUpsertTokenRate --addr=$(sekaid keys show -a validator --keyring-backend=test --home=$HOME/.sekaid) --chain-id=testing --fees=100ukex --home=$HOME/.sekaid --yes
+	// # try running upser token rate which is not allowed on poor network
+	// sekaid tx tokens upsert-rate --from validator --keyring-backend=test --denom="mykex" --rate="1.5" --fee_payments=true --chain-id=testing --fees=100ukex --home=$HOME/.sekaid  --yes
+	// # try sending more than allowed amount via bank send
+	// sekaid tx bank send validator $(sekaid keys show -a validator --keyring-backend=test --home=$HOME/.sekaid) 100000000ukex --keyring-backend=test --chain-id=testing --fees=100ukex --home=$HOME/.sekaid --yes
 }
 
 func TestIntegrationTestSuite(t *testing.T) {
--- x/gov/client/cli/util_test.go
+++ x/gov/client/cli/util_test.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	cli3 "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
-
-	"github.com/stretchr/testify/require"
-
 	"github.com/KiraCore/sekai/testutil/network"
-
 	"github.com/KiraCore/sekai/x/gov/client/cli"
 	customgovtypes "github.com/KiraCore/sekai/x/gov/types"
+	cli2 "github.com/KiraCore/sekai/x/staking/client/cli"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/testutil"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	types3 "github.com/cosmos/cosmos-sdk/types"
+	cli3 "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
+	"github.com/stretchr/testify/require"
 )
 
 // GetRolesByAddress calls the CLI command GetCmdQueryRolesByAddress and returns the roles.
@@ -96,4 +97,86 @@ func (s IntegrationTestSuite) SendValue(cCtx client.Context, from types3.AccAddr
 
 	err = s.network.WaitForNextBlock()
 	s.Require().NoError(err)
+}
+
+func (s IntegrationTestSuite) WhitelistPermission(address types.AccAddress, perm string) {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	cmd := cli.GetTxSetWhitelistPermissions()
+	out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{
+		fmt.Sprintf("--%s=%s", cli2.FlagAddr, address.String()),
+		fmt.Sprintf("--%s=%s", cli.FlagPermission, perm),
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, types3.NewCoins(types3.NewCoin(s.cfg.BondDenom, types3.NewInt(100))).String()),
+	})
+	s.Require().NoError(err)
+	fmt.Println("IntegrationTestSuite::WhitelistPermission", out.String())
+}
+
+func (s IntegrationTestSuite) VoteWithValidator0(proposalID uint64, voteOption customgovtypes.VoteOption) {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	cmd := cli.GetTxVoteProposal()
+	out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{
+		fmt.Sprintf("%d", proposalID),
+		fmt.Sprintf("%d", voteOption),
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, types3.NewCoins(types3.NewCoin(s.cfg.BondDenom, types3.NewInt(100))).String()),
+	})
+	s.Require().NoError(err)
+	var result sdk.TxResponse
+	s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(out.Bytes(), &result))
+	s.Require().NotNil(result.Height)
+}
+
+func (s IntegrationTestSuite) QueryProposals() {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	cmd := cli.GetCmdQueryProposals()
+	_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{})
+	s.Require().NoError(err)
+}
+
+func (s IntegrationTestSuite) QueryProposalVotes(proposalID uint64) {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	cmd := cli.GetCmdQueryVotes()
+	_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{
+		fmt.Sprintf("%d", proposalID),
+	})
+	s.Require().NoError(err)
+}
+
+func (s IntegrationTestSuite) QueryPoorNetworkMessages() {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	cmd := cli.GetCmdQueryPoorNetworkMessages()
+	_, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{})
+	s.Require().NoError(err)
+}
+
+func (s IntegrationTestSuite) SetPoorNetworkMessages(messages string) {
+	val := s.network.Validators[0]
+	clientCtx := val.ClientCtx
+	cmd := cli.GetTxProposalSetPoorNetworkMsgs()
+	out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{
+		messages,
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, val.Address.String()),
+		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, types3.NewCoins(types3.NewCoin(s.cfg.BondDenom, types3.NewInt(100))).String()),
+	})
+	s.Require().NoError(err)
+
+	err = s.network.WaitForNextBlock()
+	s.Require().NoError(err)
+
+	var result sdk.TxResponse
+	s.Require().NoError(val.ClientCtx.JSONMarshaler.UnmarshalJSON(out.Bytes(), &result))
+	s.Require().NotNil(result.Height)
+	s.Require().Contains(result.RawLog, "proposal-set-poor-network-messages")
 }
