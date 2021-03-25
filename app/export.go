ad4dff2c9101a7fa57ddfb793790d50e9d80c86f
frostgiant1 <frostgiant1@protonmail.com>
2021-03-25 19:04:24 +0530

    add export state

--- app/export.go
+++ app/export.go
@@ -1,12 +1,33 @@
 package app
 
 import (
+	"encoding/json"
+	customstaking "github.com/KiraCore/sekai/x/staking"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 // ExportAppStateAndValidators export the state of Sekai for a genesis file
 func (app *SekaiApp) ExportAppStateAndValidators(
 	forZeroHeight bool, jailAllowedAddrs []string,
 ) (servertypes.ExportedApp, error) {
-	return servertypes.ExportedApp{}, nil
+
+	ctx := app.NewContext(true, tmproto.Header{Height: app.LastBlockHeight()})
+
+	//todo: handle zero height upgrades
+	height := app.LastBlockHeight() + 1
+
+	genState := app.mm.ExportGenesis(ctx, app.appCodec)
+	appState, err := json.MarshalIndent(genState, "", "  ")
+	if err != nil {
+		return servertypes.ExportedApp{}, err
+	}
+
+	validators, err := customstaking.WriteValidators(ctx, app.customStakingKeeper)
+	return servertypes.ExportedApp{
+		AppState:        appState,
+		Validators:      validators,
+		Height:          height,
+		ConsensusParams: app.BaseApp.GetConsensusParams(ctx),
+	}, err
 }
