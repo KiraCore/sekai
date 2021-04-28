dd23b7d6052d9491e02b247ae866bf2c0d66f2be
fantasyup <fantasy99483@gmail.com>
2021-04-29 02:05:53 +0700

    add network properties query endpoint

--- INTERX/gateway/kira/gov.go
+++ INTERX/gateway/kira/gov.go
@@ -20,9 +20,11 @@ import (
 func RegisterKiraGovRoutes(r *mux.Router, gwCosmosmux *runtime.ServeMux, rpcAddr string) {
 	r.HandleFunc(config.QueryDataReferenceKeys, QueryDataReferenceKeysRequest(gwCosmosmux, rpcAddr)).Methods("GET")
 	r.HandleFunc(config.QueryDataReference, QueryDataReferenceRequest(gwCosmosmux, rpcAddr)).Methods("GET")
+	r.HandleFunc(config.QueryNetworkProperties, QueryNetworkPropertiesRequest(gwCosmosmux, rpcAddr)).Methods("GET")
 
 	common.AddRPCMethod("GET", config.QueryDataReferenceKeys, "This is an API to query all data reference keys.", true)
 	common.AddRPCMethod("GET", config.QueryDataReference, "This is an API to query data reference by key.", true)
+	common.AddRPCMethod("GET", config.QueryNetworkProperties, "This is an API to query network properties.", true)
 }
 
 func queryDataReferenceKeysHandle(r *http.Request, gwCosmosmux *runtime.ServeMux) (interface{}, interface{}, int) {
@@ -145,5 +147,39 @@ func QueryDataReferenceRequest(gwCosmosmux *runtime.ServeMux, rpcAddr string) ht
 		}
 
 		common.WrapResponse(w, request, *response, statusCode, common.RPCMethods["GET"][config.QueryDataReference].CachingEnabled)
+	}
+}
+
+func QueryNetworkPropertiesHandle(r *http.Request, gwCosmosmux *runtime.ServeMux) (interface{}, interface{}, int) {
+	return common.ServeGRPC(r, gwCosmosmux)
+}
+
+// QueryDataReferenceKeysRequest is a function to query data reference keys.
+func QueryNetworkPropertiesRequest(gwCosmosmux *runtime.ServeMux, rpcAddr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		request := common.GetInterxRequest(r)
+		response := common.GetResponseFormat(request, rpcAddr)
+		statusCode := http.StatusOK
+
+		common.GetLogger().Info("[query-network-properties] Entering network properties query")
+
+		if !common.RPCMethods["GET"][config.QueryNetworkProperties].Enabled {
+			response.Response, response.Error, statusCode = common.ServeError(0, "", "API disabled", http.StatusForbidden)
+		} else {
+			if common.RPCMethods["GET"][config.QueryNetworkProperties].CachingEnabled {
+				found, cacheResponse, cacheError, cacheStatus := common.SearchCache(request, response)
+				if found {
+					response.Response, response.Error, statusCode = cacheResponse, cacheError, cacheStatus
+					common.WrapResponse(w, request, *response, statusCode, false)
+
+					common.GetLogger().Info("[query-network-properties] Returning from the cache")
+					return
+				}
+			}
+
+			response.Response, response.Error, statusCode = QueryNetworkPropertiesHandle(r, gwCosmosmux)
+		}
+
+		common.WrapResponse(w, request, *response, statusCode, common.RPCMethods["GET"][config.QueryNetworkProperties].CachingEnabled)
 	}
 }
