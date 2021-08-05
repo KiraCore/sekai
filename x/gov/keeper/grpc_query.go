ac4d9ca4c551dc90a6978a22e519a38926aae990
junkai121 <junkai121@outlook.com>
2021-08-05 12:08:44 +1000

    Remove querying for all proposals to prevent management of too many proposals at a time

--- x/gov/keeper/grpc_query.go
+++ x/gov/keeper/grpc_query.go
@@ -147,9 +147,7 @@ func (k Keeper) Proposals(goCtx context.Context, request *types.QueryProposalsRe
 		request.Pagination.Limit = kiratypes.PageIterationLimit
 	}
 
-	if request.All {
-		pageRes, err = kiraquery.IterateAll(proposalsStore, request.Pagination, onResult)
-	} else if request.Reverse {
+	if request.Reverse {
 		pageRes, err = kiraquery.FilteredReversePaginate(proposalsStore, request.Pagination, onResult)
 	} else {
 		pageRes, err = query.FilteredPaginate(proposalsStore, request.Pagination, onResult)
