abd1050ddac651d3223df7562ed52f8533e8ea78
fantasyup <fantasy99483@gmail.com>
2021-12-07 09:09:14 -0500

    sync with latest master

--- INTERX/types/main.go
+++ INTERX/types/main.go
@@ -259,26 +259,6 @@ type ValidatorSigningInfo struct {
 	ProducedBlocksCounter int64  `json:"produced_blocks_counter,string"`
 }
 
-type NetworkProperties struct {
-	MinTxFee                    uint64 `json:"min_tx_fee,string,omitempty"`
-	MaxTxFee                    uint64 `json:"max_tx_fee,string,omitempty"`
-	VoteQuorum                  uint64 `json:"vote_quorum,string,omitempty"`
-	ProposalEndTime             uint64 `json:"proposal_end_time,string,omitempty"`
-	ProposalEnactmentTime       uint64 `json:"proposal_enactment_time,string,omitempty"`
-	MinProposalEndBlocks        uint64 `json:"min_proposal_end_blocks,string,omitempty"`
-	MinProposalEnactmentBlocks  uint64 `json:"min_proposal_enactment_blocks,string,omitempty"`
-	EnableForeignFeePayments    bool   `json:"enable_foreign_fee_payments,omitempty"`
-	MischanceRankDecreaseAmount uint64 `json:"mischance_rank_decrease_amount,string,omitempty"`
-	MaxMischance                uint64 `json:"max_mischance,string,omitempty"`
-	MischanceConfidence         uint64 `json:"mischance_confidence,string,omitempty"`
-	InactiveRankDecreasePercent uint64 `json:"inactive_rank_decrease_percent,string,omitempty"`
-	MinValidators               uint64 `json:"min_validators,string,omitempty"`
-	PoorNetworkMaxBankSend      uint64 `json:"poor_network_max_bank_send,string,omitempty"`
-	JailMaxTime                 uint64 `json:"jail_max_time,string,omitempty"`
-	EnableTokenWhitelist        bool   `json:"enable_token_whitelist,omitempty"`
-	EnableTokenBlacklist        bool   `json:"enable_token_blacklist,omitempty"`
-}
-
 const (
 	// GET is a constant to refer GET HTTP Method
 	GET string = "GET"
