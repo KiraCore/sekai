f55e5ea3414fc23edf7a03f489a19b8dd1bc4130
junkai121 <junkai121@outlook.com>
2022-03-01 04:27:19 +1000

    fix genesis

--- x/spending/types/genesis.go
+++ x/spending/types/genesis.go
@@ -14,7 +14,7 @@ func DefaultGenesis() *GenesisState {
 				ClaimStart:    0,
 				ClaimEnd:      0,
 				Token:         "ukex",
-				Rate:          sdk.NewDec(192_901), // 500k KEX per month
+				Rate:          sdk.NewDec(385), // 1k KEX per month per validator
 				VoteQuorum:    33,
 				VotePeriod:    300, // 300s
 				VoteEnactment: 300, // 300s
