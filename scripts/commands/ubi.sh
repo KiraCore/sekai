a38ef448e9c55bded00e755db29cb3e005661f66
junkai121 <junkai121@outlook.com>
2022-03-02 22:55:38 +1000

    further checking of ubi proposal execution edgecase

--- scripts/commands/ubi.sh
+++ scripts/commands/ubi.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sekaid query customgov network-properties
-sekaid query ubi ubi-record-by-name ValidatorQuaterlyIncome --home=$HOME/.sekaid
+sekaid query ubi ubi-record-by-name ValidatorPerMinuteIncome --home=$HOME/.sekaid
 sekaid query ubi ubi-records
+sekaid query spending pool-by-name ValidatorBasicRewardsPool --home=$HOME/.sekaid
 
 sekaid query bank balances $(sekaid keys show -a validator --keyring-backend=test --home=$HOME/.sekaid)
 
@@ -18,8 +19,8 @@ sekaid tx customgov proposal account whitelist-permission $PermVoteRemoveUBIProp
 
 # proposals
 sekaid tx customgov proposal set-network-property UBI_HARDCAP 20000000  --title="title" --description="description" --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes --broadcast-mode=block 
-sekaid tx ubi proposal-upsert-ubi --title="title" --description="description" --name="ValidatorQuaterlyIncome" --distr-start=0 --distr-end=0 --amount=2000000 --period=7776000 --pool-name="ValidatorBasicRewardsPool" --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes --broadcast-mode=block 
-sekaid tx ubi proposal-remove-ubi --title="title" --description="description" --name="ValidatorQuaterlyIncome" --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes --broadcast-mode=block 
+sekaid tx ubi proposal-upsert-ubi --title="title" --description="description" --name="ValidatorPerMinuteIncome" --distr-start=0 --distr-end=0 --amount=15 --period=60 --pool-name="ValidatorBasicRewardsPool" --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes --broadcast-mode=block 
+sekaid tx ubi proposal-remove-ubi --title="title" --description="description" --name="ValidatorPerMinuteIncome" --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes --broadcast-mode=block 
 
 sekaid query customgov proposals
 sekaid query customgov proposal 1
@@ -27,3 +28,7 @@ sekaid query customgov proposal 1
 sekaid tx customgov proposal vote 1 1 --from validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes  --broadcast-mode=block 
 sekaid tx customgov proposal vote 2 1 --from validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes  --broadcast-mode=block 
 sekaid tx customgov proposal vote 3 1 --from validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes  --broadcast-mode=block 
+
+# check ubi is only allowed for bond denom pools
+sekaid tx spending create-spending-pool --name="ValTokenRewardsPool" --claim-start=$(($(date -u +%s))) --claim-end=0 --token="valtoken" --rate=0.1 --vote-quorum="33" --vote-period="60" --vote-enactment="30" --owner-roles="" --owner-accounts=$(sekaid keys show -a validator --keyring-backend=test --home=$HOME/.sekaid) --beneficiary-roles="1" --beneficiary-accounts="" --from=validator --chain-id=testing --fees=100ukex --keyring-backend=test --home=$HOME/.sekaid --yes  --broadcast-mode=block 
+sekaid tx ubi proposal-upsert-ubi --title="title" --description="description" --name="ValidatorPerMinuteIncome" --distr-start=0 --distr-end=0 --amount=15 --period=60 --pool-name="ValTokenRewardsPool" --from=validator --keyring-backend=test --home=$HOME/.sekaid --chain-id=testing --fees=100ukex --yes --broadcast-mode=block 
