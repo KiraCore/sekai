2d90503ec48fefbc3730e9175f1886220db91375
Asmodat <asmodat@gmail.com>
2022-04-13 21:08:46 +0000

    update version

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.1.25-rc.10"
+	SekaiVersion       = "v0.1.26-rc.11"
 	CosmosVersion      = "v0.45.1"
 )
