5df0dd62d37298aa7c42c60174e625a003b7b3b2
Asmodat <asmodat@gmail.com>
2022-04-13 20:37:51 +0000

    give access to open ports

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,8 @@ set -e
 set -x
 . /etc/profile
 
+echo "INFO: Cleaning up system resources"
+kill -9 $(lsof -t -i:46147) || echo "WARNING: Nothing running on port 9090, or failed to kill processes"
+
+echo "INFO: Cleaning up system resources"
 go test -mod=readonly $(go list ./... | grep -v '/simulation')
