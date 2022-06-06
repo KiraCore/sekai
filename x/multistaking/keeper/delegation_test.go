d46970a6a157619a5d93ebe6aa820ca30ffe334b
junkai121 <junkai121@outlook.com>
2022-06-07 02:07:04 +1000

    add keeper test for pool

---
+++ x/multistaking/keeper/delegation_test.go
@@ -0,0 +1,23 @@
+package keeper_test
+
+// func (k Keeper) GetLastUndelegationId(ctx sdk.Context) uint64
+
+// func (k Keeper) SetLastUndelegationId(ctx sdk.Context, id uint64)
+
+// func (k Keeper) GetUndelegationById(ctx sdk.Context, id uint64) (undelegation types.Undelegation, found bool)
+
+// func (k Keeper) GetAllUndelegations(ctx sdk.Context) []types.Undelegation
+
+// func (k Keeper) SetUndelegation(ctx sdk.Context, undelegation types.Undelegation)
+
+// func (k Keeper) SetPoolDelegator(ctx sdk.Context, poolId uint64, delegator sdk.AccAddress)
+
+// func (k Keeper) RemovePoolDelegator(ctx sdk.Context, poolId uint64, delegator sdk.AccAddress)
+
+// func (k Keeper) GetPoolDelegators(ctx sdk.Context, poolId uint64) []sdk.AccAddress {
+
+// func (k Keeper) IncreaseDelegatorRewards(ctx sdk.Context, delegator sdk.AccAddress, amounts sdk.Coins) {
+
+// func (k Keeper) GetDelegatorRewards(ctx sdk.Context, delegator sdk.AccAddress) sdk.Coins {
+
+// func (k Keeper) IncreasePoolRewards(ctx sdk.Context, pool types.StakingPool, rewards sdk.Coins) {
