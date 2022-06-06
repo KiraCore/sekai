c3ea8321ab37a50b0385e7ccca44bc05a50c9bf7
junkai121 <junkai121@outlook.com>
2022-06-07 02:37:14 +1000

    TestLastUndelegationIdGetSet, TestUndelegationGetSet

--- x/multistaking/keeper/delegation_test.go
+++ x/multistaking/keeper/delegation_test.go
@@ -1,23 +1,72 @@
 package keeper_test
 
-// func (k Keeper) GetLastUndelegationId(ctx sdk.Context) uint64
+import (
+	"github.com/KiraCore/sekai/x/multistaking/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+)
 
-// func (k Keeper) SetLastUndelegationId(ctx sdk.Context, id uint64)
+func (suite *KeeperTestSuite) TestLastUndelegationIdGetSet() {
+	// get default last delegation id
+	lastDelegationId := suite.app.MultiStakingKeeper.GetLastUndelegationId(suite.ctx)
+	suite.Require().Equal(lastDelegationId, uint64(0))
 
-// func (k Keeper) GetUndelegationById(ctx sdk.Context, id uint64) (undelegation types.Undelegation, found bool)
+	// set last delegation id to new value
+	newDelegationId := uint64(2)
+	suite.app.MultiStakingKeeper.SetLastUndelegationId(suite.ctx, newDelegationId)
 
-// func (k Keeper) GetAllUndelegations(ctx sdk.Context) []types.Undelegation
+	// check last delegation id update
+	lastDelegationId = suite.app.MultiStakingKeeper.GetLastUndelegationId(suite.ctx)
+	suite.Require().Equal(lastDelegationId, newDelegationId)
+}
 
-// func (k Keeper) SetUndelegation(ctx sdk.Context, undelegation types.Undelegation)
+func (suite *KeeperTestSuite) TestUndelegationGetSet() {
+	addr := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	addr2 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
 
-// func (k Keeper) SetPoolDelegator(ctx sdk.Context, poolId uint64, delegator sdk.AccAddress)
+	// get undelegation by id
+	_, found := suite.app.MultiStakingKeeper.GetUndelegationById(suite.ctx, 1)
+	suite.Require().False(found)
 
-// func (k Keeper) RemovePoolDelegator(ctx sdk.Context, poolId uint64, delegator sdk.AccAddress)
+	// check whole undelegations
+	allUndelegations := suite.app.MultiStakingKeeper.GetAllUndelegations(suite.ctx)
+	suite.Require().Len(allUndelegations, 0)
 
-// func (k Keeper) GetPoolDelegators(ctx sdk.Context, poolId uint64) []sdk.AccAddress {
+	undelegations := []types.Undelegation{
+		{
+			Id:      1,
+			Address: addr.String(),
+			Expiry:  uint64(suite.ctx.BlockTime().Unix() + 1000),
+			Amount:  sdk.Coins{sdk.NewInt64Coin("ukex", 10000)},
+		},
+		{
+			Id:      2,
+			Address: addr2.String(),
+			Expiry:  uint64(suite.ctx.BlockTime().Unix() + 1000),
+			Amount:  sdk.Coins{sdk.NewInt64Coin("ukex", 10000)},
+		},
+	}
 
-// func (k Keeper) IncreaseDelegatorRewards(ctx sdk.Context, delegator sdk.AccAddress, amounts sdk.Coins) {
+	for _, undelegation := range undelegations {
+		suite.app.MultiStakingKeeper.SetUndelegation(suite.ctx, undelegation)
+	}
 
-// func (k Keeper) GetDelegatorRewards(ctx sdk.Context, delegator sdk.AccAddress) sdk.Coins {
+	// check undelegation by id
+	for _, undelegation := range undelegations {
+		p, found := suite.app.MultiStakingKeeper.GetUndelegationById(suite.ctx, undelegation.Id)
+		suite.Require().True(found)
+		suite.Require().Equal(undelegation, p)
+	}
 
-// func (k Keeper) IncreasePoolRewards(ctx sdk.Context, pool types.StakingPool, rewards sdk.Coins) {
+	// check undelegations for whole export
+	allUndelegations = suite.app.MultiStakingKeeper.GetAllUndelegations(suite.ctx)
+	suite.Require().Len(allUndelegations, 2)
+}
+
+// SetPoolDelegator(ctx sdk.Context, poolId uint64, delegator sdk.AccAddress)
+// RemovePoolDelegator(ctx sdk.Context, poolId uint64, delegator sdk.AccAddress)
+// GetPoolDelegators(ctx sdk.Context, poolId uint64) []sdk.AccAddress {
+
+// IncreasePoolRewards(ctx sdk.Context, pool types.StakingPool, rewards sdk.Coins) {
+// IncreaseDelegatorRewards(ctx sdk.Context, delegator sdk.AccAddress, amounts sdk.Coins) {
+// GetDelegatorRewards(ctx sdk.Context, delegator sdk.AccAddress) sdk.Coins {
