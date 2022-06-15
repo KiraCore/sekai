2c265838f107d659c217c7a99dac196c12055207
junkai121 <junkai121@outlook.com>
2022-06-15 08:10:59 +0800

    create release version

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.1.29-rc.14"
+	SekaiVersion       = "v0.2.0"
 	CosmosVersion      = "v0.45.1"
 )
