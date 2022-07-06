9843bbd004597702b47ea8a890a57e8ee6d86122
Kobs <sergeik30@gmail.com>
2022-07-06 15:53:37 +0200

    Custody module

--- x/custody/types/errors.go
+++ x/custody/types/errors.go
@@ -8,4 +8,5 @@ import (
 var (
 	ErrNoWhiteLists        = errors.Register(ModuleName, 2, "error getting whitelist")
 	ErrNoWhiteListsElement = errors.Register(ModuleName, 3, "error getting whitelist element")
+	ErrNotInWhiteList      = errors.Register(ModuleName, 4, "recipient not in the whitelist")
 )
