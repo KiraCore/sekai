a964a88114a34a1231ddcc6806e81173aa37ac9f
jgo121 <jgo121.k@outlook.com>
2022-11-29 22:04:04 +0800

    add a new release for kip 83 - collectives

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.3.3.27"
-	CosmosVersion      = "v0.45.1"
+	SekaiVersion       = "v0.3.4.28"
+	CosmosVersion      = "v0.45.10"
 )
