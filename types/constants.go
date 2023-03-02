bb16a90e7abf94aa511ada8096b49209a077a422
Kobs <sergeik30@gmail.com>
2023-03-02 19:23:29 +0100

    Merge latest and update cosmos version in the protocgen.sh

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.3.11.35"
+	SekaiVersion       = "v0.3.10.34"
 	CosmosVersion      = "v0.45.10"
 )
