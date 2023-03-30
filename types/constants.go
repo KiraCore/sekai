a17e7b0d5ee76a7e0fd4fcca21e1fdf206ea60ee
jgo121 <jgo121.k@outlook.com>
2023-03-30 11:11:53 +0800

    add release version for layer2 v1

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.3.12.36"
+	SekaiVersion       = "v0.3.13.37"
 	CosmosVersion      = "v0.45.10"
 )
