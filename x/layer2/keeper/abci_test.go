cdacee76212733267a82c42fb3e885988b92e4b1
jgo121 <jgo121.k@outlook.com>
2023-04-26 00:14:50 +0800

    resolve gov module test for new properties addition, add tests for new code added on layer2

--- x/layer2/keeper/abci_test.go
+++ x/layer2/keeper/abci_test.go
@@ -1,4 +1,7 @@
 package keeper_test
 
 // TODO: add test for EndBlocker
+// - finishing bootstrap case on EndBlocker
+// - operator jail case on EndBlocker
+// - liquidation case on EndBlocker
 // TODO: add test for FinishDappBootstrap
---
+++ x/layer2/keeper/lp_swap_redeem_convert_test.go
@@ -0,0 +1,16 @@
+package keeper_test
+
+// TODO: add test
+// func (k Keeper) LpTokenPrice(ctx sdk.Context, dapp types.Dapp) sdk.Dec {
+
+// TODO: add test
+// func (k Keeper) OnCollectFee(ctx sdk.Context, fee sdk.Coins) error {
+
+// TODO: add test
+// func (k Keeper) RedeemDappPoolTx(ctx sdk.Context, addr sdk.AccAddress, dapp types.Dapp, poolFee sdk.Dec, lpTokenAmount sdk.Coin) (sdk.Coin, error) {
+
+// TODO: add test
+// func (k Keeper) SwapDappPoolTx(ctx sdk.Context, addr sdk.AccAddress, dapp types.Dapp, poolFee sdk.Dec, swapBond sdk.Coin) (sdk.Coin, error) {
+
+// TODO: add test
+// func (k Keeper) ConvertDappPoolTx(
