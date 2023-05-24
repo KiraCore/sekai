52f72bb0da3ebcab3ed7eefab944903e691f38c7
jgo121 <jgo121.k@outlook.com>
2023-05-24 22:32:19 +0800

    unit test fixes & release notes

--- x/layer2/types/expected_keepers.go
+++ x/layer2/types/expected_keepers.go
@@ -12,7 +12,10 @@ type CustomGovKeeper interface {
 }
 
 type BankKeeper interface {
+	GetSupply(ctx sdk.Context, denom string) sdk.Coin
 	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
