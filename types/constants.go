ae0e1e18667a4551a9863cb06b0c52469cde1845
Kobs <sergeik30@gmail.com>
2023-07-20 10:27:00 +0200

    new version

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.3.16"
+	SekaiVersion       = "v0.3.18"
 	CosmosVersion      = "v0.45.10"
 )
