b0c31876adbc251f53c03617e7c94c99831288bd
Kobs <sergeik30@gmail.com>
2023-10-02 17:22:00 +0200

    Remove debug messages

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.3.32"
+	SekaiVersion       = "v0.4.33"
 	CosmosVersion      = "v0.45.10"
 )
