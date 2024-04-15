48c56edb233fed614020aa3cc16efcca8cfdd07f
Kobs <sergeik30@gmail.com>
2024-04-15 13:26:57 +0200

    Remove double hash system

--- types/constants.go
+++ types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	// we set page iteration limit for safety
 	PageIterationLimit = 512
-	SekaiVersion       = "v0.3.44"
+	SekaiVersion       = "v0.3.45"
 	CosmosVersion      = "v0.47.6"
 )
